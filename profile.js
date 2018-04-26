bad03a099acaacb0394b3c597f788100dcab8705
James Russo <1jlrusso@gmail.com>
2018-04-25 20:19:28 -0700

    added closeBtnContainers variable and click function to close parent window and other inner elements. added modalWindows variable and click function to close the window and inner elements. added click function to brand logo to go back to homepage, logout btn fully functional. added functionality to conversationsBtn and notificationsBtn. added variables for notifications modal, conversations modal, delivery modal, and inner elements for each with associated functions that add functionality to the inner elements like using ajax to interact with the database

--- profile.js
+++ profile.js
@@ -13,11 +13,25 @@ uploadPicBtn.addEventListener("click", function(){
 	uploadPicBtn.style.display = "none";
 })
 
-window.onclick = function(e){
-  if(e.target.matches(".modal-window") || e.target.matches(".close-btn-container") || e.target.matches(".modal-close-btn")){
-    editModalWindow.style.display = "none";
-    closeBtnContainerOne.style.display = "none";
-  }
+var closeBtnContainers = document.getElementsByClassName("close-btn-container");
+for(let i = 0; i < closeBtnContainers.length; i++){
+  closeBtnContainers[i].addEventListener("click", function(){
+    var window = this.parentElement;
+    window.style.display = "none";
+    $("body").removeClass("stop-scrolling");
+    closeOtherConvos();
+  })
+}
+
+var modalWindows = document.getElementsByClassName("modal-window");
+for(let i = 0; i < modalWindows.length; i++){
+  modalWindows[i].addEventListener("click", function(e){
+    if(e.target.matches(".modal-window")){
+      this.style.display = "none";
+      $("body").removeClass("stop-scrolling");
+      closeOtherConvos();
+    }
+  })
 }
 
 /*--- Start of User Icon JS --*/
@@ -31,6 +45,13 @@ window.onclick = function(e){
             });
         }
 
+    var goHomeBtn = document.getElementById("go-home-btn");
+    if(goHomeBtn){
+      goHomeBtn.addEventListener("click", function(){
+        document.getElementById("home-anchor").click();
+      })
+    }
+
     window.addEventListener("click", function(e){
         if(userIcon){
             if(!e.target.matches(".fa-user")){
@@ -40,7 +61,31 @@ window.onclick = function(e){
             }
         }
     });
+
+    var logoutBtn = document.getElementById("logout-btn"),
+        logoutSubmit = document.getElementById("logout-submit");
+    if(logoutBtn){
+      logoutBtn.addEventListener("click", function(){
+        logoutSubmit.click();
+      })
+    }
 /*--- End of User Icon JS ---*/
+
+var notificationsBtn = document.getElementById("my-notifications-btn"),
+    notificationsModal = document.getElementById("notifications-modal"),
+    closeBtnContainerTwo = document.getElementsByClassName("close-btn-container")[1];
+    notificationsBtn.addEventListener("click", function(){
+        notificationsModal.style.display = "block";
+        closeBtnContainerTwo.style.display = "block";
+    })
+
+var conversationsBtn = document.getElementById("my-conversations-btn"),
+    conversationsModal = document.getElementById("conversations-modal"),
+    closeBtnContainerThree = document.getElementsByClassName("close-btn-container")[2];
+    conversationsBtn.addEventListener("click", function(){
+      conversationsModal.style.display = "block";
+      closeBtnContainerThree.style.display = "block";
+    })
 
 
 
@@ -60,13 +105,6 @@ if(editInProgress){
 }
 /*--- End of Edit Order Check ---*/
 
-/*--- Check if Same Order in Progress ---*/
-var sameOrderInProgress = document.getElementById("same-order-in-progress"),
-    sameOrderInProgressVal;
-if(sameOrderInProgress){
-  sameOrderInProgressVal = sameOrderInProgress.textContent;
-}
-/*--- Check if Same Order in Progress ---*/
 
 /*--- When user clicks order completed btn ---*/
 var currentOrderRow = document.getElementById("current-order-row");
@@ -208,3 +246,399 @@ function removeThisPrevOrder($order, $btn){
   })
 }
 /*--- End of Remove Order Function ---*/
+
+
+var notificationsContainer = document.getElementById("notifications-container"),
+    notiBadge = document.getElementById("noti-badge"),
+    removeBtns = document.getElementsByClassName("close-box"),
+    denyBtns = document.getElementsByClassName("deny-btn"),
+    acceptBtns = document.getElementsByClassName("accept-btn"),
+    mailIcons = document.getElementsByClassName("mail-icon"),
+    denyOfferSubmit = document.getElementsByClassName("deny-offer-submit"),
+    removeNotiSubmit = document.getElementsByClassName("remove-noti-submit"),
+    acceptOfferSubmit = document.getElementsByClassName("accept-offer-submit"),
+    denyBtnsLen = denyBtns.length,
+    removeBtnsLen = removeBtns.length,
+    acceptBtnsLen = acceptBtns.length,
+    sendMessageForms = document.getElementsByClassName("send-message-form"),
+    closeNotiContainers = document.getElementsByClassName("close-noti-container"),
+    closeBarOnes = document.getElementsByClassName("close-bar-one"),
+    closeBarTwos = document.getElementsByClassName("close-bar-two");
+
+if(notificationsBtn){
+  notificationsBtn.addEventListener("click", function(){
+      notificationsModal.style.display = "block";
+      $(".close-btn-container").css("display", "block");
+      $("body").addClass("stop-scrolling");
+      getNewNotifications();
+  })
+}
+
+function acceptOfferFunc(){
+  if(acceptBtns){
+    for(let i = 0; i < acceptBtns.length; i++){
+      acceptBtns[i].addEventListener("click", function(){
+        acceptOfferSubmits[i].click();
+      })
+    }
+  }
+}
+function denyOfferFunc(){
+  if(denyBtns){
+    for(let i = 0; i < denyBtns.length; i++){
+      denyBtns[i].addEventListener("click", function(){
+        denyOfferSubmits[i].click();
+      })
+    }
+  }
+}
+
+function setMailBtnFunc(){
+  if(mailIcons){
+    for(let i = 0; i < mailIcons.length; i++){
+      mailIcons[i].addEventListener("click", function(){
+        sendMessageForms[i].classList.toggle("toggle-send-message-form");
+        removeBtns[i].classList.toggle("toggle-close-box");
+        mailIcons[i].classList.toggle("toggle-move-up");
+        closeBarOnes[i].classList.toggle("toggle-move-up");
+        closeBarTwos[i].classList.toggle("toggle-move-up");
+      })
+    }
+  }
+}
+
+
+function setRemoveBtnFunc(){
+  if(removeBtns){
+    for(let i = 0; i < removeBtns.length; i++){
+      removeBtns[i].addEventListener("click", function(){
+        removeNotiSubmits[i].click();
+      })
+    }
+  }
+}
+
+function getNewNotifications(){
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "includes/getNewNotifications.php", true);
+  xhr.onload = function(){
+    if(this.status == 200){
+      var data = this.responseText;
+      if(data){
+        $("#modal-body").append(data);
+      }
+    }
+    acceptOfferFunc();
+    denyOfferFunc();
+    setMailBtnFunc();
+    setRemoveBtnFunc();
+  }
+  xhr.send();
+}
+
+function checkNewNotifications(){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "includes/checkNewNotifications.php", true);
+  xhr.onload = function(){
+    if(this.status == 200){
+      var data = this.responseText;
+      if(data){
+        if(noNotifications){
+          noNotifications.style.display = "none";
+        }
+        notiBadge.style.opacity = "1";
+      } else {
+        notiBadge.style.opacity = "0";
+      }
+    }
+  }
+  xhr.send();
+}
+
+setInterval(checkNewNotifications, 2000);
+
+
+/*--- Conversations --*/
+/*-- Conversations Modal --*/
+var orderItemsLength = document.getElementsByClassName("order-item").length,
+    deliveryOrderInners = document.getElementsByClassName("delivery-order-inner"),
+    conversationsBtn = document.getElementById("my-conversations-btn"),
+    conversationsModal = document.getElementById("conversations-modal"),
+    convoBodyWrapper = document.getElementById("convo-body-wrapper"),
+    closeBtnContainerThree = document.getElementsByClassName("close-btn-container")[2],
+    viewConvos = document.getElementsByClassName("view-convo"),
+    regMsgReadSubmits = document.getElementsByClassName("reg-msg-read-submit"),
+    messagesOuters = document.getElementsByClassName("messages-outer"),
+    messagesInners = document.getElementsByClassName("messages-inner");
+
+
+if(conversationsBtn){
+    conversationsBtn.addEventListener("click", function(){
+    conversationsModal.style.display = "block";
+    closeBtnContainerThree.style.display = "block";
+    convoBodyWrapper.style.left = 0;
+    $("body").addClass("stop-scrolling");
+    $(".close-btn-container").css("display", "block");
+  })
+}
+
+var convoBadge = document.getElementById("convo-badge");
+setInterval(getAllConvoBtns, 2000);
+
+
+function getAllConvoBtns(){
+  if(viewConvos){
+    for(let i = 0; i < viewConvos.length; i++){
+      var senderId = viewConvos[i].getAttribute("sender");
+      checkForNewMsgs(senderId);
+    }
+  }
+}
+
+function checkForNewMsgs(senderId){
+  var xhr = new XMLHttpRequest();
+  var param = "sender=" + senderId + "";
+  xhr.open("POST", "includes/checkForNewMsgs.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function(){
+    if(this.status == 200){
+      var data = this.responseText;
+      if(data){
+        convoBadge.style.opacity = "1";
+        $("#" + senderId + "-user").find(".new-msg-alert").css("display", "block");
+      } else {
+        convoBadge.style.opacity = "0";
+        $("#" + senderId + "-user").find(".new-msg-alert").css("display", "none");
+      }
+    }
+  }
+  xhr.send(param);
+}
+
+var msgInt;
+var convoCheckboxes = document.getElementsByClassName("convo-checkbox");
+if(viewConvos){
+  for(let i = 0; i < viewConvos.length; i++){
+    viewConvos[i].addEventListener("click", function(){
+      closeOtherConvos(i);
+      scrollMsgsToBottom();
+      var senderId = this.getAttribute("sender");
+      uncheckOtherBoxes(i, senderId);
+      var thisCheckbox = convoCheckboxes[i];
+      if(thisCheckbox.checked == false){
+        thisCheckbox.checked = true;
+      } else if(thisCheckbox.checked == true){
+        thisCheckbox.checked = false;
+      }
+      checkBoxFunc(thisCheckbox, senderId, i);
+      this.classList.toggle("toggle-view-convo");
+      messagesOuters[i].classList.toggle("toggle-messages-outer");
+    })
+  }
+}
+
+function uncheckOtherBoxes(thisNum, senderId){
+  for(let i = 0; i < convoCheckboxes.length; i++){
+    if(i != thisNum){
+      convoCheckboxes[i].checked = false;
+      var thisCheckbox = convoCheckboxes[i];
+      checkBoxFunc(thisCheckbox, senderId);
+    }
+  }
+}
+
+function checkBoxFunc(thisCheckbox, senderId){
+  if(thisCheckbox.checked == true){
+    msgInt = setInterval(function(){
+      console.log("getting msgs from " + senderId);
+      getNewMessages(senderId);
+    }, 2000);
+  } else if(thisCheckbox.checked == false){
+    console.log("stop log");
+    stopLog();
+  }
+}
+function stopLog(){
+  clearInterval(msgInt);
+}
+
+conversationsModal.addEventListener("click", function(e){
+  if(e.target.matches("#conversations-modal")){
+    for(let i = 0; i < convoCheckboxes.length; i++){
+      convoCheckboxes[i].checked = false;
+      var thisCheckbox = convoCheckboxes[i];
+      checkBoxFunc(thisCheckbox);
+    }
+    console.log("modal-clicked");
+  }
+})
+
+$(".close-btn-container").click(function(){
+  for(let i = 0; i < convoCheckboxes.length; i++){
+    convoCheckboxes[i].checked = false;
+    var thisCheckbox = convoCheckboxes[i];
+    checkBoxFunc(thisCheckbox);
+  }
+  console.log("close clicked");
+})
+
+
+
+function getNewMessages(senderId){
+  var xhr = new XMLHttpRequest();
+  var sender = "sender=" + senderId;
+  xhr.open("POST", "includes/getNewMessages.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function(){
+    if(this.status == 200){
+      var data = this.responseText;
+      if(data){
+        $("#with-user-" + senderId + "").append(data);
+      }
+    }
+  }
+  xhr.send(sender);
+}
+
+function scrollMsgsToBottom(){
+  for(let i = 0; i < messagesInners.length; i++){
+    messagesInners[i].scrollTop = messagesInners[i].scrollHeight;
+  }
+}
+
+function closeOtherConvos(thisNum){
+  for(let i = 0; i < messagesOuters.length; i++){
+    if(i != thisNum){
+      if(messagesOuters[i].classList.contains("toggle-messages-outer")){
+        messagesOuters[i].classList.remove("toggle-messages-outer");
+      }
+      if(viewConvos[i].classList.contains("toggle-view-convo")){
+        viewConvos[i].classList.remove("toggle-view-convo");
+      }
+    }
+  }
+}
+
+
+
+var sendMsgSubmits = document.getElementsByClassName("send-message-submit");
+if(sendMsgSubmits){
+  for(let i = 0; i < sendMsgSubmits.length; i++){
+    sendMsgSubmits[i].addEventListener("click", function(e){
+      e.preventDefault();
+      var $otherId = $(this).parent(".text-submit-container").siblings(".hidden").attr("value");
+      var $message = $(this).siblings(":text").val();
+      sendAjaxMsg($otherId, $message);
+      $(this).siblings(":text").val("");
+    })
+  }
+}
+
+var convoSendSubmits = document.getElementsByClassName("convo-send-submit");
+if(convoSendSubmits){
+  for(let i = 0; i < convoSendSubmits.length; i++){
+    convoSendSubmits[i].addEventListener("click", function(e){
+      e.preventDefault();
+      var $otherId = $(this).siblings(".hidden").attr("value");
+      var $message = $(this).siblings(":text").val();
+      if($message == ""){
+        alert("cannot send blank");
+      } else {
+        sendAjaxMsg($otherId, $message);
+        $(this).siblings(":text").val("");
+      }
+    })
+  }
+}
+
+function sendAjaxMsg(otherId, message){
+  var xhr = new XMLHttpRequest();
+  var params = "other-id=" + otherId + "&message=" + message + "";
+  xhr.open("POST", "includes/sendAjaxMsg.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onload = function(){
+    if(this.status == 200){
+      var data = this.responseText;
+      if(data){
+        $("#with-user-" + otherId + "").append(data);
+        scrollMsgsToBottom();
+      }
+    }
+  }
+  scrollMsgsToBottom();
+  xhr.send(params);
+}
+
+
+/*-- Deliveries Modal --*/
+var myDeliveriesBtn = document.getElementById("my-deliveries"),
+    myDeliveriesModal = document.getElementById("my-deliveries-modal"),
+    myDeliveriesContainer = document.getElementById("my-deliveries-container"),
+    deliveryCancelBtns = document.getElementsByClassName("delivery-cancel-btn"),
+    deliveryCancelSubmits = document.getElementsByClassName("delivery-cancel-submit"),
+    deliveryCompletedBtns = document.getElementsByClassName("delivery-completed-btn"),
+    deliveryCompletedSubmits = document.getElementsByClassName("delivery-completed-submit");
+
+if(myDeliveriesBtn){
+  myDeliveriesBtn.addEventListener("click", function(){
+    myDeliveriesModal.style.display = "block";
+    $(".close-btn-container").css("display", "block");
+    myDeliveriesContainer.style.top = "50%";
+    $("body").addClass("stop-scrolling");
+  })
+}
+
+if(deliveryCancelBtns){
+  for(let i = 0; i < deliveryCancelBtns.length; i++){
+    deliveryCancelBtns[i].addEventListener("click", function(){
+      deliveryCancelSubmits[i].click();
+    })
+  }
+}
+
+if(deliveryCompletedBtns){
+  for(let i = 0; i < deliveryCompletedBtns.length; i++){
+    deliveryCompletedBtns[i].addEventListener("click", function(){
+      deliveryCompletedSubmits[i].click();
+    })
+  }
+}
+
+var deliveryMsgSubmits = document.getElementsByClassName("delivery-msg-submit");
+if(deliveryMsgSubmits){
+  for(let i = 0; i < deliveryMsgSubmits.length; i++){
+    deliveryMsgSubmits[i].addEventListener("click", function(e){
+      e.preventDefault();
+      var $otherId = $(this).siblings(".hidden").attr("value");
+      var $message = $(this).siblings(":text").val();
+      if($message == ""){
+        alert("cannot send blank");
+      } else {
+        sendAjaxMsg($otherId, $message);
+        $(this).siblings(":text").val("");
+      }
+    })
+  }
+}
+
+window.addEventListener("resize", function(){
+  var windowWidth = window.innerWidth;
+  if(windowWidth < 1250 && windowWidth > 1185){
+    var orderItems = document.getElementsByClassName("order-item");
+    var elmtWidth = document.getElementsByClassName("order-store-date")[0].offsetWidth;
+    for(let i = 0; i < orderItems.length; i++){
+      orderItems[i].style.width = (elmtWidth - 0.1) + "px";
+    }
+  }
+})
+
+window.addEventListener("load", function(){
+  var windowWidth = window.innerWidth;
+  if(windowWidth < 1250 && windowWidth > 1185){
+    var orderItems = document.getElementsByClassName("order-item");
+    var elmtWidth = document.getElementsByClassName("order-store-date")[0].offsetWidth;
+    for(let i = 0; i < orderItems.length; i++){
+      orderItems[i].style.width = (elmtWidth) + "px";
+    }
+  }
+})
